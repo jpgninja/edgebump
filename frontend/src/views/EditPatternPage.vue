729839324e2ef7e2021450382c24c2f6225d5064
Chris Mewhort <chrismewhort@gmail.com>
2025-09-17 16:24:26 -0700

    removes unneeded comment

--- frontend/src/views/EditPatternPage.vue
+++ frontend/src/views/EditPatternPage.vue
@@ -1,12 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue";
     const pattern = ref({})
-    // onMounted(async () => {
-    //     pattern.value = await (await fetch("http://localhost:3000/api/pattern/{id}", {
-    //     headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` }
-    // })).json()
-    // })
-
     import EditPattern from "../components/EditPattern.vue";
 </script>
 
