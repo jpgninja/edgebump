cf7ddef6abc048b264a7ee683efab49034b73817
Chris Mewhort <chrismewhort@gmail.com>
2025-09-17 11:09:55 -0700

    renames project to edgebump

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <h1 class="text-3xl text-white font-bold mb-6">TradeStack</h1>
+    <h1 class="text-3xl text-white font-bold mb-6">EdgeBump</h1>
     <p>we da best.</p>
     <router-link to="/login" class="text-blue-400 hover:underline">Login</router-link>
 </template>
